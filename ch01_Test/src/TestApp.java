cf689a3b307aae2833fadccee99198971f8662e2
Bryan Menendez Sabla <menendbm@gmail.com>
2021-02-11 09:44:26 -0500

    init commit

---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,13 @@
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		System.out.println("Success!");
+	
+		System.out.println("Success!"); 
+		System.out.println("Success!");
+		
+
+	}
+
+}
---
+++ ch02_calculator/src/CalculatorApp.java
@@ -0,0 +1,46 @@
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println("CalculatorApp");
+		// integer arithmetic
+		
+		int n1 = 5;
+		int n2 = 8;
+		int n3 = 21;
+		int n4 = 4;
+		
+		int sum1 = n1 + n2; 
+		System.out.println(sum1);
+		
+		int diff1 = n2 - n4;
+		System.out.println(diff1);
+		
+		int prod1 = n3 * n1;
+		System.out.println(prod1);
+		
+		int quot1 = n3 / n1;
+		System.out.println(quot1);
+		
+		
+		// double arithmetic 
+		double d1 = 5.5;
+		double d2 = 7.0;
+		double d3 = 11.1;
+		double d4 = 17.2;
+		
+		double sum2 = d1 + d3;
+		System.out.println(sum2);
+		
+		double diff2 = d4 - d1;
+		System.out.println(diff2);
+		
+		double prod2 = d3 * d2;
+		System.out.println(prod2);
+		
+		double quotient2 = d4 / d1;
+		System.out.println(quotient2);
+				
+	}
+
+}
